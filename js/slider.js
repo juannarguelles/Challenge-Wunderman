a0b4a28aaf3166f7edd70507c981ca329a772b91
unknown <juannarguelles@gmail.com>
2021-08-10 15:00:33 -0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+function createSlick(){  
+	$(".carousel__inner").not('.slick-initialized').slick({
+		centerMode: true,
+	    autoplay: true,
+	    dots: true, 
+        arrows: true,
+  		slidesToShow: 3,
+	    responsive: [{ 
+	        breakpoint: 768,
+	        settings: {
+	            dots: false,
+	            arrows: false,
+	            infinite: true,
+	            slidesToShow: 1,
+	            slidesToScroll: 1
+	        } 
+	    }]
+	});	
+} 
+createSlick();
+$(window).on( 'resize', createSlick );
